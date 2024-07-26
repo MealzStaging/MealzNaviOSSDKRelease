29debeb176c3702f2bb46288a687b3dedcca5380
Thomas Lemay <thomas.lemay@miam.tech>
2024-07-26 08:58:18 +0200

    update release

--- Package.swift
+++ Package.swift
@@ -14,20 +14,17 @@ let package = Package(
     products: [
         .library(
             name: "MealzNaviOSSDK",
-            targets: ["MealzNaviOSSDK"]),
+            targets: ["MealzNaviOSSDK"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/miamtech/MealzIOSFrameworkRelease", from: "1.0.0")
+        .package(url: "https://github.com/MealzStaging/MealzUIiOSSDKRelease", from: "10.0.0"),
     ],
     targets: [
         .binaryTarget(
             name: "MealzNaviOSSDK",
-            url: "https://github.com/miamtech/MealzNaviOSSDKStaging/raw/release/1.0.0/MealzNaviOSSDK.zip",
-            checksum: "81854e6754d097bb3dc64ec564f1430896f3538be6ee18def68fd9cdfd8f7745"
-        )
-        
+            url: "https://github.com/MealzStaging/MealzNaviOSSDKRelease/raw/release/10.0.2/MealzNaviOSSDK.zip",
+            checksum: "e3b971e3c8f637fd77bfde6e1d149d4a6776db3fb76c261c7cc3a7b2a2c0039c"
+        ),
     ]
 )
-/*
- 
- */
